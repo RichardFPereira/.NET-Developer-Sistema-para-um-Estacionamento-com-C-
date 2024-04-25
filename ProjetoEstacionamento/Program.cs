f0bb4436374f9a03c6c09e07053bdcdeeb89a15e
Richard <68912027+RichardFPereira@users.noreply.github.com>
2024-04-25 13:47:48 +0000

    Mudança de estrutura das pastas

---
+++ ProjetoEstacionamento/Program.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoEstacionamento.Models;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+Estacionamento smartPark = new Estacionamento(precoInicial, precoPorHora);
+
+Console.WriteLine("\nBem vindo ao Smart Park!\n");
+
+Console.WriteLine("Digite o preço inicial: ");
+precoInicial = smartPark.ValidaValor();
+
+Console.WriteLine("Digite o preço por hora: ");
+precoPorHora = smartPark.ValidaValor();
+
+Console.WriteLine("Preço inicial: " + precoInicial);
+Console.WriteLine("Preço por hora: " + precoPorHora);
+
