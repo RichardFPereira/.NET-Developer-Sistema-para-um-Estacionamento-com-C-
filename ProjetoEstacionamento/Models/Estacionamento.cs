c510cbd6563354d998462c54cf71aaac4656263a
Richard <68912027+RichardFPereira@users.noreply.github.com>
2024-04-25 21:41:45 +0000

    Criados os métodos RemoverVeículo, ListarVeiculo, CalcularValor na classe `Estacionamento.cs`

--- ProjetoEstacionamento/Models/Estacionamento.cs
+++ ProjetoEstacionamento/Models/Estacionamento.cs
@@ -15,6 +15,7 @@ namespace ProjetoEstacionamento.Models
             this.precoPorHora = precoPorHora;
         }
 
+        //Adiciona novo veículo
         public void AdicionarVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo que deseja estacionar: ");
@@ -26,11 +27,56 @@ namespace ProjetoEstacionamento.Models
             }
             else
             {
-                Console.WriteLine("A placa é inválida!");
+                Console.Clear();
+                Console.WriteLine("A placa é inválida!");                
                 AdicionarVeiculo();
             }
         }
 
+        //Remove veículo já existente
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo que deseja remover: ");
+            string placa = Console.ReadLine();
+            
+            if (ValidaPlaca(placa))
+            {
+                if (veiculos.Contains(placa))
+                {
+                    veiculos.Remove(placa);            
+                    Console.WriteLine($"Veículo de placa {placa} removido com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("O veículo com a placa especificada não está estacionado.");
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("A placa é inválida!");
+                RemoverVeiculo();
+            }       
+        }
+
+        //Lista veículos existentes
+        public void ListarVeiculos()
+        {
+            if (veiculos.Count == 0)
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+            else
+            {
+                Console.WriteLine("Veículos estacionados:");
+                foreach (string placa in veiculos)
+                {
+                    Console.WriteLine(placa);
+                }
+            }
+        }
+
+        //Verifica se a placa é válida
         static bool ValidaPlaca(string placa)
         {
             string regexPlaca = @"^([A-Za-z]{3}-\d{4}|[A-Za-z]{3}\d[A-Za-z]\d{2})$";
@@ -38,6 +84,8 @@ namespace ProjetoEstacionamento.Models
             return Regex.IsMatch(placa, regexPlaca);
 
         }
+
+        //Verifica se o valor digitado é válido
         public decimal ValidaValor()
         {
             decimal valor = 0;
@@ -52,6 +100,14 @@ namespace ProjetoEstacionamento.Models
                     Console.WriteLine("Valor inválido! \nDigite novamente o valor inicial: ");                    
                 }
             }
+        }
+
+        public void CalcularValor(decimal valorInicial, decimal valorPorHora)
+        {
+            Console.WriteLine("Digite quantas horas o veículo ficou estacionado.");
+            decimal horas = Convert.ToDecimal(Console.ReadLine());
+            decimal valorFinal = (valorInicial + (valorPorHora * horas));
+            Console.WriteLine($"O valor total foi de: R$ {valorFinal:F2}.");
         }
     }
 }
