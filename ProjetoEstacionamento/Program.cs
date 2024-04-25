0c1a8dcd375fddf40b1bede52c63ceb4888b1802
Richard <68912027+RichardFPereira@users.noreply.github.com>
2024-04-25 14:14:13 +0000

    Adicionado menu inicial com chamadas aos métodos Adicionar, Remover e Listar

--- ProjetoEstacionamento/Program.cs
+++ ProjetoEstacionamento/Program.cs
@@ -12,6 +12,37 @@ precoInicial = smartPark.ValidaValor();
 Console.WriteLine("Digite o preço por hora: ");
 precoPorHora = smartPark.ValidaValor();
 
-Console.WriteLine("Preço inicial: " + precoInicial);
-Console.WriteLine("Preço por hora: " + precoPorHora);
+// Console.WriteLine("Preço inicial: " + precoInicial);
+// Console.WriteLine("Preço por hora: " + precoPorHora);
 
+string escolha = "";
+bool menu = true;
+
+while (menu)
+{
+    Console.Clear();
+    Console.WriteLine("Sistema Smart Park - Escolha uma das opções abaixo: ");
+    Console.WriteLine("1 - Cadastrar veículo;");
+    Console.WriteLine("2 - Remover veículo;");
+    Console.WriteLine("3 - Listar veículo;");
+    Console.WriteLine("4 - Encerrar;");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            smartPark.AdicionarVeiculo();
+            break;
+        
+        case "2":
+            smartPark.RemoverVeiculo();
+            break;
+        
+        case "3":
+            smartPark.ListarVeiculos();
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
